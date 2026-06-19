7223f8998ed2adbb437046b14aff20da4ff4ea08
Carlos-Avellaneda-2 <carlos.avellaneda-f@mail.escuelaing.edu.co>
2026-06-18 23:33:19 -0500

    first commit

---
+++ src/main/java/eci/dosw/alpha/BienestarService/BienestarServiceApplication.java
@@ -0,0 +1,13 @@
+package eci.dosw.alpha.BienestarService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BienestarServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BienestarServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eci/dosw/alpha/BienestarService/BienestarServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package eci.dosw.alpha.BienestarService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BienestarServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
